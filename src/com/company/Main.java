d8ab0b8645b74a896e1b06f2bbf7969e5edeb0bf
kahi88-max <mjumabaev22@gmail.com>
2021-05-12 16:51:18 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        BankAccount bankAccount = new BankAccount();
+        bankAccount.deposit(20000);
+        System.out.println("Вашем счете:"+bankAccount.getAmount()+"\nСколько хотите снять счета?");
+        Scanner scanner = new Scanner(System.in);
+        int sum = scanner.nextInt();
+
+        while (true){
+            try{
+                System.out.println(bankAccount.getAmount());
+                bankAccount.withdraw(sum);
+            } catch (LimitException e) {
+                System.out.println(e.getMessage());
+                try {
+                    bankAccount.withdraw((int) bankAccount.getAmount());
+                } catch (LimitException ex) {
+                    ex.printStackTrace();
+                }
+                break;
+            }
+        }
+    }
+}
